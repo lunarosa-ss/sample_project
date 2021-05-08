35f5c3357c89cdc7a4c8a471b725de804eaf61f2
lunarosa-ss <lunaorsa@live.jp>
2021-05-08 14:54:27 +0900

    4th commit, Update: :memo: クレジットロゴ画像サイズ修正

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -12,15 +12,15 @@
         Hostname : <?php $cmd = '/bin/hostname'; echo exec($cmd); ?><br>
         OS : Amazon Linux2
         <hr>
-		<p>
-		<li> <a href="https://docs.aws.amazon.com/ja_jp/codecommit/latest/userguide/welcome.html"  target="_blank">AWS CodeCommit</a>
-		<li> <a href="https://docs.aws.amazon.com/ja_jp/codedeploy/latest/userguide/welcome.html"  target="_blank">AWS CodeDeploy</a>
-		<li> <a href="https://docs.aws.amazon.com/ja_jp/codepipeline/latest/userguide/welcome.html"  target="_blank">AWS CodePipeline</a>
-		<li> <a href="https://docs.aws.amazon.com/ja_jp/codebuild/latest/userguide/welcome.html"  target="_blank">AWS CodeBuild</a>
-		<hr>
-		<p>
-		<li> <a href=./phpinfo.php>PHP Info</a></li>
-		<hr>
-		<P><img src="images/group-enrise-relations.png" alt="enrise-relations" width="100.00" height="40.75"></p> <small>@2021 ENRISE RELATIONS INC.</small>
+        <p>
+        <li> <a href="https://docs.aws.amazon.com/ja_jp/codecommit/latest/userguide/welcome.html"  target="_blank">AWS CodeCommit</a>
+        <li> <a href="https://docs.aws.amazon.com/ja_jp/codedeploy/latest/userguide/welcome.html"  target="_blank">AWS CodeDeploy</a>
+        <li> <a href="https://docs.aws.amazon.com/ja_jp/codepipeline/latest/userguide/welcome.html"  target="_blank">AWS CodePipeline</a>
+        <li> <a href="https://docs.aws.amazon.com/ja_jp/codebuild/latest/userguide/welcome.html"  target="_blank">AWS CodeBuild</a>
+        <hr>
+        <p>
+        <li> <a href=./phpinfo.php>PHP Info</a></li>
+        <hr>
+        <P><img src="images/group-enrise-relations.png" alt="enrise-relations" width="133.3" height="43.0"></p> <small>@2021 ENRISE RELATIONS INC.</small>
     </body>
 </html>
