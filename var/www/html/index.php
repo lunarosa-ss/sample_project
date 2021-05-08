4c044815a32ec74c871b9412975a7da2e186cd4e
lunarosa-ss <lunaorsa@live.jp>
2021-05-08 15:50:12 +0900

    5th commit, Update: :memo: GitHub リンク表示

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -13,13 +13,16 @@
         OS : Amazon Linux2
         <hr>
         <p>
-        <li> <a href="https://docs.aws.amazon.com/ja_jp/codecommit/latest/userguide/welcome.html"  target="_blank">AWS CodeCommit</a>
-        <li> <a href="https://docs.aws.amazon.com/ja_jp/codedeploy/latest/userguide/welcome.html"  target="_blank">AWS CodeDeploy</a>
-        <li> <a href="https://docs.aws.amazon.com/ja_jp/codepipeline/latest/userguide/welcome.html"  target="_blank">AWS CodePipeline</a>
-        <li> <a href="https://docs.aws.amazon.com/ja_jp/codebuild/latest/userguide/welcome.html"  target="_blank">AWS CodeBuild</a>
+			<li> <a href=./phpinfo.php>PHP Info</a></li>
+		<hr>
+		<p>
+			<li> <a href="https://docs.aws.amazon.com/ja_jp/codecommit/latest/userguide/welcome.html"  target="_blank">AWS CodeCommit</a>
+			<li> <a href="https://docs.aws.amazon.com/ja_jp/codedeploy/latest/userguide/welcome.html"  target="_blank">AWS CodeDeploy</a>
+			<li> <a href="https://docs.aws.amazon.com/ja_jp/codepipeline/latest/userguide/welcome.html"  target="_blank">AWS CodePipeline</a>
+			<li> <a href="https://docs.aws.amazon.com/ja_jp/codebuild/latest/userguide/welcome.html"  target="_blank">AWS CodeBuild</a>
         <hr>
         <p>
-        <li> <a href=./phpinfo.php>PHP Info</a></li>
+			<li> <a href=https://github.com/>GitHub.com</a></li>
         <hr>
         <P><img src="images/group-enrise-relations.png" alt="enrise-relations" width="133.3" height="43.0"></p> <small>@2021 ENRISE RELATIONS INC.</small>
     </body>
