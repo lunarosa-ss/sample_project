b072e4c6fd5767f265f8b38e5b9a5e3702cd10f4
lunarosa-ss <lunaorsa@live.jp>
2021-05-08 14:27:59 +0900

    2nd commit, Add: :wrench: .github/workflows/main.yml

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -11,13 +11,17 @@
         Date : <?php $cmd = '/bin/date'; echo exec($cmd); ?><br>
         Hostname : <?php $cmd = '/bin/hostname'; echo exec($cmd); ?><br>
         OS : Amazon Linux2
-		<p>
-		<!-- <li> <a href=./phpinfo.php>PHP Info</a></li> -->
         <hr>
 		<p>
+		<li> <a href="https://docs.aws.amazon.com/ja_jp/codecommit/latest/userguide/welcome.html"  target="_blank">AWS CodeCommit</a>
 		<li> <a href="https://docs.aws.amazon.com/ja_jp/codedeploy/latest/userguide/welcome.html"  target="_blank">AWS CodeDeploy</a>
 		<li> <a href="https://docs.aws.amazon.com/ja_jp/codepipeline/latest/userguide/welcome.html"  target="_blank">AWS CodePipeline</a>
+		<li> <a href="https://docs.aws.amazon.com/ja_jp/codebuild/latest/userguide/welcome.html"  target="_blank">AWS CodeBuild</a>
 		<hr>
-		<P>ENRISE SOLUTION
+		<p>
+		<li> <a href=./phpinfo.php>PHP Info</a></li>
+		<hr>
+		<P><img src="images/group-enrise-solution.png" alt="enrise-solution" width="100.00" height="40.75"></p> <small>@2021 ENRISE SOLUTION, Inc.</small>
     </body>
 </html>
+
